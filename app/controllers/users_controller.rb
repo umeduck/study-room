7438fe4c0f6437f54f7ea3f34a3ec089c6dd1e9d
umeduck <ume_duck@outlook.jp>
2022-10-25 18:32:02 +0900

    rubocopの実施

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @school_year = SchoolYear.find(@user.school_year_id)
   end
+
   def edit
   end
+
   def update
     if current_user.update(user_params)
       redirect root_path
@@ -14,6 +16,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :school_year_id)
   end
